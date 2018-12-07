4a8626ee3403241dab479adae7cfd4be06a4bebc
divya <hello@shortdiv.com>
2018-12-07 12:03:01 -0600

    fix: Update url in state

--- src/state/modules/auth/state.js
+++ src/state/modules/auth/state.js
@@ -1,7 +1,7 @@
 import GoTrue from "gotrue-js";
 
 export const auth = new GoTrue({
-  APIUrl: "https://clever-williams-469dd0.netlify.com/.netlify/identity",
+  APIUrl: "https://netlify-gotrue-in-vue.netlify.com/.netlify/identity",
   audience: "",
   setCookie: false
 });
